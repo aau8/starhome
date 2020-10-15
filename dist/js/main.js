2798fa4dfd7ce67e5340e57672b9b74fbda4331e
artemyg0811 <artiom.ygryumov@gmail.com>
2020-10-16 00:41:03 +0300

    -

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,8 +5,7 @@ let menu = document.querySelector('.navbar__menu');
 
 function menuToggle() {
   menu.classList.toggle('_active');
-  document.querySelector('.navbar__menu-bg').classList.toggle('_show');
-  // document.querySelector('body').classList.toggle('_lock');
+  document.querySelector('body').classList.toggle('_lock');
 }
 
 burger.addEventListener('click', function(e) {
@@ -27,18 +26,3 @@ document.addEventListener('mouseup', function(e) {
     menuToggle();
   }
 });
-
-// document.addEventListener('scroll', () => { 
-//   let scrollTop = window.scrollY;
-
-//   let navbarDown = document.querySelector('.navbar');
-//   // let navbarUp = document.querySelector('.navbar__up');
-
-//   if(scrollTop >= 100){
-//     navbarDown.classList.add('_show');
-//     // navbarUp.classList.add('_hide');
-//   }else{    
-//     navbarDown.classList.remove('_show');
-//     // navbarUp.classList.remove('_hide');
-//   }
-// });
