1e9b9db3ea2f40c43b5070f7f9cc6ee7ef4d118d
artemyg0811 <artiom.ygryumov@gmail.com>
2020-10-16 14:52:36 +0300

    Сделал форму в header

--- dist/js/main.js
+++ dist/js/main.js
@@ -26,3 +26,20 @@ document.addEventListener('mouseup', function(e) {
     menuToggle();
   }
 });
+
+// ФОРМА В HEADER
+let tabOrdinary = document.querySelector('.header__form__tab-ordinary');
+let tabGroup = document.querySelector('.header__form__tab-group');
+
+tabOrdinary.addEventListener('click', function() {
+  this.classList.add('_active');
+  tabGroup.classList.remove('_active');
+  document.querySelector('.header__booking').style.display = 'block';
+  document.querySelector('.header__application').style.display = 'none';
+});
+tabGroup.addEventListener('click', function() {
+  this.classList.add('_active');
+  tabOrdinary.classList.remove('_active');
+  document.querySelector('.header__application').style.display = 'block';
+  document.querySelector('.header__booking').style.display = 'none';
+});
